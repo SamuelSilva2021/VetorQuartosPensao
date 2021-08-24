7f11fab2058ef85fb5ff6fd8e95d9d425f408067
Samuca <79856359+SamuelSilva2021@users.noreply.github.com>
2021-08-24 17:24:14 -0300

    Adicionar arquivos de projeto.

---
+++ VetorQuartosPensao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VetorQuartosPensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] vet = new Estudantes[10];
+
+            Console.WriteLine("Quantos quartos serao alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int num = int.Parse(Console.ReadLine());
+                vet[num] = new Estudantes(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+            }
+        }
+    }
+}
---
+++ VetorQuartosPensao/Quarto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorQuartosPensao
+{
+    class Quarto
+    {
+        public string Nome;
+        public string Email;
+        public int Numero;
+    }
+}
